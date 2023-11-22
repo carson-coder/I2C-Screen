b552143b4a3a0026ba630d9e6449b866d2c00190
carson-coder <carsondpool@gmail.com>
2023-11-22 18:31:24 -0500

    Fix WDT Error on ESP8266

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,22 @@
 
 void loop() {
     //show_text(get_serial_data());
+    wifi_loop();
 }
 
 void setup() {
     Serial.begin(9600); 
+    Serial.setDebugOutput(true);
+    struct rst_info *rtc_info = system_get_rst_info();
+    os_printf("reset reason: %x\n", rtc_info->reason);
+    if (rtc_info->reason == REASON_WDT_RST ||
+    rtc_info->reason == REASON_EXCEPTION_RST ||
+    rtc_info->reason == REASON_SOFT_WDT_RST) {
+        if (rtc_info->reason == REASON_EXCEPTION_RST) {
+            os_printf("Fatal exception (%d):\n", rtc_info->exccause);
+        }
+        os_printf("epc1=0x%08x, epc2=0x%08x, epc3=0x%08x, excvaddr=0x%08x, depc=0x%08x\n", rtc_info->epc1, rtc_info->epc2, rtc_info->epc3, rtc_info->excvaddr, rtc_info->depc);
+    }
     screen_setup();
     //serial_setup();
     wifi_setup(ssid, password);
--- src/wifi.h
+++ src/wifi.h
@@ -7,10 +7,18 @@ using namespace std;
 
 String tmp;
 
+String _SSID;
+const char * _PASS;
+
 void wifi_setup(String SSID, const char* PASS) {
+    _SSID = SSID;
+    _PASS = PASS;
     WiFi.begin(SSID, PASS);
     show_text("Connecting to " + SSID);
+}
 
-    while (WiFi.status() != WL_CONNECTED) {}
-    show_text("Connected to" + SSID + "\nIp: " + WiFi.localIP().toString());
+void wifi_loop() {
+    if (WiFi.status() == WL_CONNECTED) {
+        show_text("Connected to " + _SSID + "\nIp: " + WiFi.localIP().toString());
+    }
 }
