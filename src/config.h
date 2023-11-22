03829ca20e73873e847cde203a6c25093dadd4da
carson-coder <carsondpool@gmail.com>
2023-11-22 16:44:04 -0500

    fix

--- src/config.h
+++
@@ -1,4 +0,0 @@
-#include <Arduino.h>
-
-String ssid = "SSID";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "Password";     // The password of the Wi-Fi network
